2e3d2020e76399901d4d9bc96a6c01634791e0c4
Joonhee Jeong <jeonggoo75@gmail.com>
2020-10-22 16:43:35 +0900

    feat: solve boj 12865

---
+++ backjoon/12865.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+
+struct Stuff {
+    int w, v;
+    Stuff(int w, int v) : w(w), v(v) {}
+};
+
+inline int max(int a, int b) { return a >= b ? a : b; }
+
+struct Knapsack {
+    int n, k;
+    std::vector<Stuff> stuffs;
+    std::vector<std::vector<int>> cache;
+    Knapsack(int n, int k);
+    int solve(int i, int cap);
+    int solve();
+};
+
+Knapsack::Knapsack(int n, int k) : n(n), k(k)
+{
+    int w, v;
+    int temp = n;
+    while (temp--) {
+        std::cin >> w >> v;
+        stuffs.push_back(std::move(Stuff(w,v)));
+        cache.push_back(std::move(std::vector<int>(k+1, -1)));
+    }
+}
+
+int Knapsack::solve(int i, int cap)
+{
+    if (i == n) return 0;
+    int &ret = cache[i][cap];
+    if (ret != -1) return ret;
+    ret = solve(i+1, cap);
+    const int &w = stuffs[i].w;
+    const int &v = stuffs[i].v;
+    if (w <= cap && v != 0)
+        ret = max(ret, v + solve(i+1, cap-w));
+    return ret;
+}
+
+int Knapsack::solve()
+{
+    return solve(0, k);
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int n, k;
+    std::cin >> n >> k;
+    Knapsack knapsack(n, k);
+    std::cout << knapsack.solve() << std::endl;
+    return 0;
+}
